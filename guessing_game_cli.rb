6d27f8a69e77fde8dafb097ceed049b3a2c75e2d
Skye Geraghty <skyetay03@googlemail.com>
2020-02-13 11:37:26 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,16 @@
 # Code your solution here!
+def run_gessing_game
+  
+end
+
+def get_user_input
+  return gets.chomp
+end
+
+def prompt_user
+  puts "Please pick a number between 1 - 6"
+end
+
+def get_number
+  return rand(5) + 1
+end
